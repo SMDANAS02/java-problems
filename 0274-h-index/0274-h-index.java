c30b85ee3b3f15592780e10947bcb9d2c04764d6
Mohamed Anas <smohamedanas02@gmail.com>
2026-06-30 20:58:58 +0530

    Time: 32 ms (5.1%), Space: 43.3 MB (73.9%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int sum=0;
+        for(int i=1;i<=citations.length;i++){
+             int count=0;
+             for(int j=0;j<citations.length;j++){
+                if(citations[j]>=i){
+                    count++;
+                }
+                if(count>=i){
+                    sum=i;
+                }
+
+
+             }
+        }
+        return sum;
+    }
+}
