cf7c60932f469acccf37e976657e750262282e21
Mohamed Anas <smohamedanas02@gmail.com>
2026-08-01 22:25:58 +0530

    Time: 0 ms (100%), Space: 42.1 MB (59.29%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int n=x^y,count=0;
+        while(n>0){
+            count+=(n&1);
+            n=n>>1;
+        }
+        return count;
+    }
+}
